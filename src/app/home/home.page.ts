3c72465e19ff28263e7e3a30f0146141e71484cb
Muhammad Faris <deployments@muhdfaris.com>
2026-06-25 22:21:53 +0800

    chore: upgrade dependencies

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  changeDetection: ChangeDetectionStrategy.Eager,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
