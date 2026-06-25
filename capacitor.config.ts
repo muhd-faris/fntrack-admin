f23019175b0d52e7b0a7c67d7da5f0417b3ecaef
Muhammad Faris <deployments@muhdfaris.com>
2026-06-25 22:05:47 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.muhdfaris.fntrack.admin',
+  appName: 'fntrack-admin',
+  webDir: 'www'
+};
+
+export default config;
